990c17d57c64cc1563a569af3138911a2902795e
Micael-Rodrigues <134837371+m1caelvr@users.noreply.github.com>
2024-11-28 14:46:54 +0000

    Update

--- Bot.py
+++ Bot.py
@@ -1,3 +1,5 @@
+import asyncio
+from screeninfo import get_monitors
 import pydirectinput as pdi
 import pandas as pd
 import pyperclip
@@ -5,20 +7,32 @@ import pyautogui
 import keyboard
 import logging
 import time
-import os
+from Class.shared_state import SharedState
+
+monitor = get_monitors()[0]
+
+original_width = 1920
+original_height = 1080
+new_width = monitor.width
+new_height = monitor.height
+
+scale_x = new_width / original_width
+scale_y = new_height / original_height
+
+SOURCE_FILE = "PREVENTIVAS.xlsx"
 
 COORDINATES = {
-    "CLICK_TO_INSERT_OS_N": (452, 278),
-    "CLICK_OUTSIDE_THE_INPUT_N": (316, 330),
-    "CLICK_TO_ADD_DOC_N": (1755, 216),
-    "CLICK_TO_ADD_FILE_N": (1677, 155),
-    "INSERT_NAME_FILE_N": (1647, 503),
-    "CLICK_INPUT_N": (1723, 289),
-    "CLICK_IN_FILE_N": (985, 409),
-    "CLICK_IN_OK_N": (1721, 674),
-    "CLICK_PROCEDURE_OS_N": (917, 176),
-    "CLICK_INPUT_RADIO_HEADER_N": (1172, 255),
-    "CLICK_ON_LABOR_N": (609, 181),
+    "CLICK_TO_INSERT_OS": (452, 278),
+    "CLICK_OUTSIDE_THE_INPUT": (316, 330),
+    "CLICK_TO_ADD_DOC": (1755, 216),
+    "CLICK_TO_ADD_FILE": (1677, 155),
+    "INSERT_NAME_FILE": (1647, 503),
+    "CLICK_INPUT": (1723, 289),
+    "CLICK_IN_FILE": (985, 409),
+    "CLICK_IN_OK": (1721, 674),
+    "CLICK_PROCEDURE_OS": (917, 176),
+    "CLICK_INPUT_RADIO_HEADER": (1172, 255),
+    "CLICK_ON_LABOR": (609, 181),
     "CLICK_ADD_LINE": (435, 253),
     "INITIAL_SERVICE_DATE": (1055, 318),
     "FINAL_SERVICE_DATE": (1212, 319),
@@ -35,6 +49,27 @@ CONSTANTS = {
     "HN": "HN",
 }
 
+COORDINATES_2 = {
+    "CLICK_TO_INSERT_OS": (273,224),
+    "CLICK_OUTSIDE_THE_INPUT": (214,268),
+    "CLICK_TO_ADD_DOC": (1220,171),
+    "CLICK_TO_ADD_FILE": (1096,128),
+    "INSERT_NAME_FILE": (1144,402),
+    "CLICK_INPUT": (1200,232),
+    "CLICK_IN_FILE": (686,251),
+    "CLICK_IN_OK": (1200,515),
+    "CLICK_PROCEDURE_OS": (699,145),
+    "CLICK_INPUT_RADIO_HEADER": (822,206),
+    "CLICK_ON_LABOR": (442,145),
+    "CLICK_ADD_LINE": (295,205),
+    "INITIAL_SERVICE_DATE": (811,262),
+    "FINAL_SERVICE_DATE": (927,257),
+    "CLICK_SAVE": (1063,118),
+    "SEARCH_OS_STATE": (391,383),
+    "CLICK_OS_CONCLUDE": (361,317),
+    "CLICK_END_SERVICE": (692,482),
+}
+
 logging.basicConfig(
     level=logging.INFO,
     filename="automacao.log",
@@ -42,70 +77,93 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
-SOURCE_FILE = "PREVENTIVAS.xlsx"
-
+def log_stop_execution():
+    print(f"stop_execution: {SharedState.stop_execution}")
+    
 
 def sleep(seconds):
+    if SharedState.stop_execution:
+        logging.info("Execução interrompida.")
+        return
+    
+    log_stop_execution()
+    
     time.sleep(seconds)
 
-
 def safe_click(coords):
+    if SharedState.stop_execution:
+        logging.info("Execução interrompida.")
+        return
+    
+    log_stop_execution()
+
     pdi.moveTo(*coords)
     pdi.click()
     sleep(0.5)
 
-
 def keyboard_pressed(key):
+    if SharedState.stop_execution:
+        logging.info("Execução interrompida.")
+        return
+
+    log_stop_execution()
+    
     pdi.press(key)
 
-
 def hotkey(*keys):
+    if SharedState.stop_execution:
+        logging.info("Execução interrompida.")
+        return
+
     for key in keys[:-1]:
         pdi.keyDown(key)
     pdi.press(keys[-1])
     for key in keys[:-1]:
         pdi.keyUp(key)
 
+def finalize_line(df, line_index, file_name="PREVENTIVAS.xlsx"):
+    try:
+        if line_index in df.index:
+            df.at[line_index, "Status"] = "Finalizada"
+            
+            df.to_excel(file_name, index=False)
+            logging.info(f"Planilha atualizada com sucesso: {file_name}")
 
-def wait_until_found(image, timeout=10):
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        location = pyautogui.locateOnScreen(image, confidence=0.9)
-        if location:
-            return location
-        sleep(0.5)
-    raise TimeoutError(f"Imagem {image} não encontrada em {timeout} segundos.")
+        else:
+            logging.warning(f"Índice {line_index} não encontrado no DataFrame.")
 
+    except Exception as e:
+        logging.error(f"Erro ao finalizar linha {line_index}: {e}")
 
 def insert_os(cell_value):
-    safe_click(COORDINATES["CLICK_TO_INSERT_OS_N"])
+    safe_click(COORDINATES_2["CLICK_TO_INSERT_OS"])
     hotkey("ctrl", "a")
     pyautogui.write(str(cell_value))
-    safe_click(COORDINATES["CLICK_OUTSIDE_THE_INPUT_N"])
+    safe_click(COORDINATES_2["CLICK_OUTSIDE_THE_INPUT"])
     sleep(4.5)
 
-
 def add_doc():
-    safe_click(COORDINATES["CLICK_TO_ADD_DOC_N"])
-    safe_click(COORDINATES["CLICK_TO_ADD_FILE_N"])
+    safe_click(COORDINATES_2["CLICK_TO_ADD_DOC"])
+    safe_click(COORDINATES_2["CLICK_TO_ADD_FILE"])
     pyperclip.copy(CONSTANTS["FILE_IN_PRISMA_NAME"])
-    safe_click(COORDINATES["INSERT_NAME_FILE_N"])
+    safe_click(COORDINATES_2["INSERT_NAME_FILE"])
     hotkey("ctrl", "v")
-    safe_click(COORDINATES["CLICK_INPUT_N"])
-    safe_click(COORDINATES["CLICK_IN_FILE_N"])
-    safe_click(COORDINATES["CLICK_IN_OK_N"])
+    safe_click(COORDINATES_2["CLICK_INPUT"])
+    sleep(.5)
+    safe_click(COORDINATES_2["CLICK_IN_FILE"])
+    pyautogui.press("enter")
+    safe_click(COORDINATES_2["CLICK_IN_OK"])
     sleep(1)
-
 
 def fill_data():
-    safe_click(COORDINATES["CLICK_OUTSIDE_THE_INPUT_N"])
+    safe_click(COORDINATES_2["CLICK_OUTSIDE_THE_INPUT"])
     sleep(0.5)
-    safe_click(COORDINATES["CLICK_PROCEDURE_OS_N"])
+    safe_click(COORDINATES_2["CLICK_PROCEDURE_OS"])
     sleep(1)
-    safe_click(COORDINATES["CLICK_INPUT_RADIO_HEADER_N"])
-    sleep(2)
-    safe_click(COORDINATES["CLICK_ON_LABOR_N"])
-    safe_click(COORDINATES["CLICK_ADD_LINE"])
+    safe_click(COORDINATES_2["CLICK_INPUT_RADIO_HEADER"])
+    sleep(2.5)
+    safe_click(COORDINATES_2["CLICK_ON_LABOR"])
+    safe_click(COORDINATES_2["CLICK_ADD_LINE"])
 
     sleep(1)
     pyautogui.write("EQS")
@@ -125,64 +183,77 @@ def fill_data():
     sleep(1.5)
     pyautogui.write(CONSTANTS["HN"])
     sleep(1)
-    safe_click(COORDINATES["CLICK_OUTSIDE_THE_INPUT_N"])
+    safe_click(COORDINATES_2["CLICK_OUTSIDE_THE_INPUT"])
     sleep(0.5)
 
+def end_service():
+    safe_click(COORDINATES_2["CLICK_SAVE"])
+    sleep(4)
+    safe_click(COORDINATES_2["CLICK_OUTSIDE_THE_INPUT"])
+    sleep(4.5)
+    safe_click(COORDINATES_2["SEARCH_OS_STATE"])
+    sleep(0.5)
+    safe_click(COORDINATES_2["CLICK_OS_CONCLUDE"])
+    sleep(1.5)
+    safe_click(COORDINATES_2["CLICK_SAVE"])
+    sleep(1)
+    safe_click(COORDINATES_2["CLICK_END_SERVICE"])
+    sleep(4)
 
 def process_lines(df):
     df["Status"] = df["Status"].astype(str)
 
     for index, row in df.iterrows():
-        if keyboard.is_pressed("esc"):
-            logging.info("Script interrompido pelo usuário.")
-            print("Script interrompido pelo usuário!")
+        if SharedState.stop_execution:
+            logging.info("Execução interrompida.")
             break
 
         cell_value = row.iloc[0]
         status_value = row["Status"]
 
         logging.info(
-            f"Processando linha {index + 1}: {cell_value} | Status: {status_value}"
+            f"Processando linha {index + 2}: {cell_value} | Status: {status_value}"
         )
 
         try:
             if status_value != "Finalizada":
+
+                if SharedState.stop_execution:
+                    logging.info("Execução interrompida.")
+                    break
                 insert_os(cell_value)
+
+                if SharedState.stop_execution:
+                    logging.info("Execução interrompida.")
+                    break
                 add_doc()
+
+                if SharedState.stop_execution:
+                    logging.info("Execução interrompida.")
+                    break
                 fill_data()
 
-                safe_click(COORDINATES["CLICK_SAVE"])
-                sleep(4)
-                safe_click(COORDINATES["CLICK_OUTSIDE_THE_INPUT_N"])
-                sleep(4.5)
-                safe_click(COORDINATES["SEARCH_OS_STATE"])
-                sleep(0.5)
-                safe_click(COORDINATES["CLICK_OS_CONCLUDE"])
-                sleep(1.5)
-                safe_click(COORDINATES["CLICK_SAVE"])
-                sleep(1)
-                safe_click(COORDINATES["CLICK_END_SERVICE"])
-                sleep(4)
+                if SharedState.stop_execution:
+                    logging.info("Execução interrompida.")
+                    break
+                end_service()
 
-                df.at[index, "Status"] = "Finalizada"
-                logging.info(f"Linha {index + 1} - Status atualizado para: Finalizada")
-                print(f"Linha {index + 1} - Status atualizado para: Finalizada")
+                if SharedState.stop_execution:
+                    logging.info("Execução interrompida.")
+                    break
+                finalize_line(df, index)
+                
             else:
-                logging.info(f"Linha {index + 1} já está finalizada. Pulando...")
+                logging.info(f"Linha {index + 2} já está finalizada. Pulando...")
 
         except Exception as e:
-            logging.error(f"Erro ao processar linha {index + 1}: {e}")
-            print(f"Erro ao processar linha {index + 1}: {e}")
-
-    df.to_excel(SOURCE_FILE, index=False)
-    logging.info("Arquivo Excel atualizado com o status das OSs.")
+            logging.error(f"Erro ao processar linha {index + 2}: {e}")
+            print(f"Erro ao processar linha {index + 2}: {e}")
 
 
-if __name__ == "__main__":
-    try:
+async def start_bot():
+    try: 
         logging.info("Iniciando o script de automação...")
-        print("Pressione 'ESC' para interromper o script.")
-
         df = pd.read_excel(SOURCE_FILE)
 
         if "Status" not in df.columns:
---
+++ Flet_interface.py
@@ -0,0 +1,102 @@
+import flet as ft
+import logging
+import asyncio
+import tracemalloc
+from Class.shared_state import SharedState
+from Bot import start_bot
+
+tracemalloc.start()
+
+class ScriptController:
+    def __init__(self, page):
+        self.page = page
+        self.script_task = None
+        self.start_button = None
+        self.stop_button = None
+        
+    async def run_script(self):
+        try:
+            logging.info("Iniciando o script de automação...")
+            await start_bot()
+        except asyncio.CancelledError:
+            logging.info("Script foi interrompido.")
+            print("Script foi interrompido.")
+        except Exception as e:
+            logging.error(f"Erro crítico: {e}")
+            print(f"Erro crítico: {e}")
+            
+            snapshot = tracemalloc.take_snapshot()
+            top_stats = snapshot.statistics('lineno')
+            print("[ Top 10 Memory Allocation Lines ]")
+            for stat in top_stats[:10]:
+                print(stat)
+        finally:
+            await self.reset_ui()
+
+    async def start_script(self, e):
+        SharedState.stop_execution = False
+        self.start_button.disabled = True
+        self.stop_button.disabled = False
+        await asyncio.sleep(.5)
+
+        await self.page.update_async()
+        
+        if self.script_task and not self.script_task.done():
+            self.script_task.cancel()
+            try:
+                await self.script_task
+            except asyncio.CancelledError:
+                pass
+        
+        self.script_task = asyncio.create_task(self.run_script())
+        
+    async def stop_script(self, e):
+        print("Entrou no stop_script")
+        SharedState.stop_execution = True
+        logging.info("Execução interrompida pelo botão 'Interromper'.")
+        
+        if self.script_task and not self.script_task.done():
+            self.script_task.cancel()
+            try:
+                await self.script_task
+            except asyncio.CancelledError:
+                print("Tarefa cancelada com sucesso")
+    
+    async def reset_ui(self):
+        self.start_button.disabled = False
+        self.stop_button.disabled = True
+        await asyncio.sleep(.5)
+
+        await self.page.update_async()
+
+def main(page: ft.Page):
+    page.window.width = 250
+    page.window.height = 150
+    page.title = "OS_BOT"
+    page.horizontal_alignment = "center"
+    page.window.always_on_top = True
+
+    controller = ScriptController(page)
+
+    async def start_script_wrapper(e):
+        await controller.start_script(e)
+
+    async def stop_script_wrapper(e):
+        await controller.stop_script(e)
+
+    controller.start_button = ft.ElevatedButton(
+        "Iniciar Script", 
+        on_click=start_script_wrapper
+    )
+    controller.stop_button = ft.ElevatedButton(
+        "Interromper Script", 
+        on_click=stop_script_wrapper, 
+        disabled=True
+    )
+
+    page.add(controller.start_button, controller.stop_button)
+    page.update()
+
+    page.on_close = lambda _: tracemalloc.stop()
+
+ft.app(target=main)
